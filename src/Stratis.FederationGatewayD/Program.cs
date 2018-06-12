85519f55456b8073f37727161a7db496870b9e71
Matthieu Le Berre <monsieur.leberre@gmail.com>
2018-06-12 15:25:34 +0100

    improve the way we start gateways up

--- src/Stratis.FederationGatewayD/Program.cs
+++ src/Stratis.FederationGatewayD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using NBitcoin;
 using NBitcoin.Protocol;
@@ -14,11 +15,15 @@ using Stratis.Bitcoin.Features.RPC;
 using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Utilities;
 using Stratis.FederatedPeg.Features.FederationGateway;
+using Stratis.Networks.Apex;
 
 namespace Stratis.FederationGatewayD
 {
     class Program
     {
+        private const string MainchainArgument = "-mainchain";
+        private const string SidechainArgument = "-sidechain";
+
         static void Main(string[] args)
         {
             RunFederationGatewayAsync(args).Wait();
@@ -28,10 +33,14 @@ namespace Stratis.FederationGatewayD
         {
             try
             {
-                Network network = Network.StratisRegTest;
+                var isMainchainNode = args.FirstOrDefault(a => a.ToLower() == MainchainArgument) != null;
+                var isSidechainNode = args.FirstOrDefault(a => a.ToLower() == SidechainArgument) != null;
+                if(isSidechainNode == isMainchainNode) throw new ArgumentException(
+                    $"Gateway node needs to be started specifiying either a {SidechainArgument} or a {MainchainArgument} argument");
+
+                var network = isMainchainNode ? Network.StratisTest : ApexNetwork.Test;
                 NodeSettings nodeSettings = new NodeSettings(network, ProtocolVersion.ALT_PROTOCOL_VERSION, args: args);
-
-
+                
                 // NOTES: running BTC and STRAT side by side is not possible yet as the flags for serialization are static
                 var node = new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
