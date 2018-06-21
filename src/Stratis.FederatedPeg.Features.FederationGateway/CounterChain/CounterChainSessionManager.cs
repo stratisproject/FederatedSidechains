b8332bc78711a0b47acd3406d9d4e4d477ca4f0e
Jeremy Bokobza <bokobza@gmail.com>
2018-06-21 01:13:59 +0100

    Commented out some bits

--- src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/CounterChainSessionManager.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/CounterChain/CounterChainSessionManager.cs
@@ -168,23 +168,23 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.CounterChain
             this.logger.LogTrace("({0}:'{1}',{2}:'{3}',{4}:'{5}')", nameof(sessionId), sessionId, nameof(amount), amount, nameof(destinationAddress), destinationAddress);            
             this.logger.LogInformation("ProcessCounterChainSession: Session Registered.");
 
-            // Check if this has already been done then we just return the transactionId
-            if (this.sessions.TryGetValue(sessionId, out var counterchainSession))
-            {
-                // This is the mechanism that tells the round robin not to continue and also
-                // notifies the monitorChain of the completed transactionId from the counterChain transaction.
-                if (counterchainSession.CounterChainTransactionId != uint256.Zero)
-                {
-                    // If we get here:
-                    // 1. One of the nodes became the boss and successfully broadcast a completed transaction.
-                    // 2. The monitor in this node received the block with the transaction (identified by the sessionId in the op_return).
-                    // 3. The monitor wrote the CounterChainTransactionId into the counterChainSession to indicate all was done.
-                    // This method then does not try to process the transaction and instead signals to the monitorChain that this
-                    // transaction already completed by passing back the transactionId.
-                    this.logger.LogInformation($"Counterchain Session: {sessionId} was already completed. Doing nothing.");
-                    return counterchainSession.CounterChainTransactionId;
-                }
-            }
+            //// Check if this has already been done then we just return the transactionId
+            //if (this.sessions.TryGetValue(sessionId, out var counterchainSession))
+            //{
+            //    // This is the mechanism that tells the round robin not to continue and also
+            //    // notifies the monitorChain of the completed transactionId from the counterChain transaction.
+            //    if (counterchainSession.CounterChainTransactionId != uint256.Zero)
+            //    {
+            //        // If we get here:
+            //        // 1. One of the nodes became the boss and successfully broadcast a completed transaction.
+            //        // 2. The monitor in this node received the block with the transaction (identified by the sessionId in the op_return).
+            //        // 3. The monitor wrote the CounterChainTransactionId into the counterChainSession to indicate all was done.
+            //        // This method then does not try to process the transaction and instead signals to the monitorChain that this
+            //        // transaction already completed by passing back the transactionId.
+            //        this.logger.LogInformation($"Counterchain Session: {sessionId} was already completed. Doing nothing.");
+            //        return counterchainSession.CounterChainTransactionId;
+            //    }
+            //}
 
             //create the partial transaction template
             var wallet = this.federationWalletManager.GetWallet();
