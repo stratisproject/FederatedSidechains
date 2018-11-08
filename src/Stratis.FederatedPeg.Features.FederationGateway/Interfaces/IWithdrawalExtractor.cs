e0f0c21c536d709f4bf466b76fe14322a4d7a4b7
Ferdeen Mughal <Ferdeen.Mughal@StratisPlatform.com>
2018-11-08 11:12:41 +0000

    Use cref for types mentioned in the documentation.

--- src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IWithdrawalExtractor.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IWithdrawalExtractor.cs
@@ -6,7 +6,7 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
     /// <summary>
     /// This component is responsible for finding all deposits made from the federation's
     /// multisig address to a target address, find out if they represent a cross chain transfer
-    /// and if so, extract the details into an IWithdrawal.
+    /// and if so, extract the details into an <see cref="IWithdrawal"/>.
     /// </summary>
     public interface IWithdrawalExtractor
     {
