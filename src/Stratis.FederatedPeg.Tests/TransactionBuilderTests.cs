0a061dc235ebe3b52159f7cf7f2b36622885dd1c
quantumagi <someguy.fromafrica@gmail.com>
2018-11-14 17:33:37 +1100

    Refactor TransactionBuilderTest

--- src/Stratis.FederatedPeg.Tests/TransactionBuilderTests.cs
+++ src/Stratis.FederatedPeg.Tests/TransactionBuilderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -37,9 +38,13 @@ namespace Stratis.FederatedPeg.Tests
         public TransactionBuilderTests()
         {
             this.network = ApexNetwork.RegTest;
+            string dataDir = GetTestDirectoryPath(this);
+            if (Directory.Exists(dataDir))
+                Directory.Delete(dataDir, true);
             this.nodeSettings = new NodeSettings(this.network, NBitcoin.Protocol.ProtocolVersion.ALT_PROTOCOL_VERSION,
                 args: new[] {
                     "-mainchain",
+                    $"-datadir={dataDir}",
                     "-redeemscript=2 026ebcbf6bfe7ce1d957adbef8ab2b66c788656f35896a170257d6838bda70b95c 02a97b7d0fad7ea10f456311dcd496ae9293952d4c5f2ebdfc32624195fde14687 02e9d3cd0c2fa501957149ff9d21150f3901e6ece0e3fe3007f2372720c84e3ee1 03c99f997ed71c7f92cf532175cea933f2f11bf08f1521d25eb3cc9b8729af8bf4 034b191e3b3107b71d1373e840c5bf23098b55a355ca959b968993f5dec699fc38 5 OP_CHECKMULTISIG",
                     "-publickey=026ebcbf6bfe7ce1d957adbef8ab2b66c788656f35896a170257d6838bda70b95c"
                 });
@@ -66,59 +71,46 @@ namespace Stratis.FederatedPeg.Tests
 
             // Create the wallet.
             this.wallet = (this.federationWalletManager as FederationWalletManager).GenerateWallet();
+            (this.federationWalletManager as FederationWalletManager).Wallet = this.wallet;
 
-            var mockFederationWalletManager = new Mock<IFederationWalletManager>();
-
-            // Return the mock wallet.
-            mockFederationWalletManager.Setup(s => s.GetWallet()).Returns(this.wallet);
-
-            // Mock funds in wallet.
-            mockFederationWalletManager.Setup(s => s.GetSpendableTransactionsInWallet(It.IsAny<int>())).Returns(new[]
+            this.wallet.MultiSigAddress.Transactions.Add(new TransactionData()
             {
-                new UnspentOutputReference()
-                {
-                    Transaction = new TransactionData()
-                    {
-                        Amount = Money.COIN * 90,
-                        Id = new uint256(1),
-                        Index = 0,
-                        ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
-                        BlockHeight = 2
-                    }
-                },
-                new UnspentOutputReference()
-                {
-                    Transaction = new TransactionData()
-                    {
-                        Amount = Money.COIN * 80,
-                        Id = new uint256(1),
-                        Index = 1,
-                        ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
-                        BlockHeight = 2
-                    }
-                },
-                new UnspentOutputReference()
-                {
-                    Transaction = new TransactionData()
-                    {
-                        Amount = Money.COIN * 70,
-                        Id = new uint256(2),
-                        Index = 0,
-                        ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
-                        BlockHeight = 3
-                    }
-                }
+                Amount = Money.COIN * 90,
+                Id = new uint256(1),
+                Index = 0,
+                ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
+                BlockHeight = 2
             });
 
-            this.federationWalletManager2 = mockFederationWalletManager.Object;
-            this.federationTransactionHandler = new FederationWalletTransactionHandler(this.loggerFactory, this.federationWalletManager2, this.walletFeePolicy, this.network);
+            this.wallet.MultiSigAddress.Transactions.Add(new TransactionData()
+            {
+                Amount = Money.COIN * 80,
+                Id = new uint256(1),
+                Index = 1,
+                ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
+                BlockHeight = 2
+            });
+
+            this.wallet.MultiSigAddress.Transactions.Add(new TransactionData()
+            {
+                Amount = Money.COIN * 70,
+                Id = new uint256(2),
+                Index = 0,
+                ScriptPubKey = this.wallet.MultiSigAddress.ScriptPubKey,
+                BlockHeight = 2
+            });
+
+            (this.federationWalletManager as FederationWalletManager).LoadKeysLookupLock();
+
+            this.federationTransactionHandler = new FederationWalletTransactionHandler(this.loggerFactory, this.federationWalletManager, this.walletFeePolicy, this.network);
         }
 
         [Fact]
         public void BuildTransactionBuildsDeterministicTransactions()
         {
             MultiSigAddress multiSigAddress = this.wallet.MultiSigAddress;
-            var recipients = new List<Recipient.Recipient>()
+
+            var recipient1 = new List<Recipient.Recipient>()
             {
                 new Recipient.Recipient
                 {
@@ -128,13 +120,14 @@ namespace Stratis.FederatedPeg.Tests
             };
 
             int blockHeight = this.chain.Height;
-            string opReturnData = blockHeight.ToString();
+
+            string opReturnData1 = blockHeight.ToString();
 
             // Build the multisig transaction template.
-            var multiSigContext = new TransactionBuildContext(recipients, opReturnData: Encoding.UTF8.GetBytes(opReturnData))
+            var multiSigContext1 = new TransactionBuildContext(recipient1, opReturnData: Encoding.UTF8.GetBytes(opReturnData1))
             {
                 TransactionFee = Money.Coins(0.01m),
-                MinConfirmations = 1,
+                MinConfirmations = 0,
                 Shuffle = false,
                 MultiSig = multiSigAddress,
                 IgnoreVerify = true,
@@ -142,29 +135,95 @@ namespace Stratis.FederatedPeg.Tests
             };
 
             // Build the transaction.
-            Transaction transaction = this.federationTransactionHandler.BuildTransaction(multiSigContext);
+            Transaction transaction1 = this.federationTransactionHandler.BuildTransaction(multiSigContext1);
 
             // Transactions inputs.
-            Assert.Equal(2, transaction.Inputs.Count);
-            Assert.Equal((uint256)1, transaction.Inputs[0].PrevOut.Hash);
-            Assert.Equal((uint)0, transaction.Inputs[0].PrevOut.N);
-            Assert.Equal((uint256)1, transaction.Inputs[1].PrevOut.Hash);
-            Assert.Equal((uint)1, transaction.Inputs[1].PrevOut.N);
+            Assert.Equal(2, transaction1.Inputs.Count);
+            Assert.Equal((uint256)1, transaction1.Inputs[0].PrevOut.Hash);
+            Assert.Equal((uint)0, transaction1.Inputs[0].PrevOut.N);
+            Assert.Equal((uint256)1, transaction1.Inputs[1].PrevOut.Hash);
+            Assert.Equal((uint)1, transaction1.Inputs[1].PrevOut.N);
 
             // Transaction outputs.
-            Assert.Equal(3, transaction.Outputs.Count);
+            Assert.Equal(3, transaction1.Outputs.Count);
 
             // Transaction output value - change.
-            Assert.Equal(new Money(9.99m, MoneyUnit.BTC), transaction.Outputs[0].Value);
-            Assert.Equal(multiSigAddress.ScriptPubKey, transaction.Outputs[0].ScriptPubKey);
+            Assert.Equal(new Money(9.99m, MoneyUnit.BTC), transaction1.Outputs[0].Value);
+            Assert.Equal(multiSigAddress.ScriptPubKey, transaction1.Outputs[0].ScriptPubKey);
 
-            // Transaction output value - recipient.
-            Assert.Equal(new Money(160m, MoneyUnit.BTC), transaction.Outputs[1].Value);
-            Assert.Equal(recipients[0].ScriptPubKey, transaction.Outputs[1].ScriptPubKey);
+            // Transaction output value - recipient 1.
+            Assert.Equal(new Money(160m, MoneyUnit.BTC), transaction1.Outputs[1].Value);
+            Assert.Equal(recipient1[0].ScriptPubKey, transaction1.Outputs[1].ScriptPubKey);
 
             // Transaction output value - op_return.
-            Assert.Equal(new Money(0m, MoneyUnit.BTC), transaction.Outputs[2].Value);
-            Assert.Equal(opReturnData, new OpReturnDataReader(this.loggerFactory, this.network).GetString(transaction, out OpReturnDataType dummy));
+            Assert.Equal(new Money(0m, MoneyUnit.BTC), transaction1.Outputs[2].Value);
+            Assert.Equal(opReturnData1, new OpReturnDataReader(this.loggerFactory, this.network).GetString(transaction1, out OpReturnDataType dummy));
+
+            var recipient2 = new List<Recipient.Recipient>()
+            {
+                new Recipient.Recipient
+                {
+                    Amount = Money.COIN * 70,
+                    ScriptPubKey = PayToPubkeyHashTemplate.Instance.GenerateScriptPubKey(new Key().PubKey.Hash)
+                }
+            };
+
+            // Reserve UTXO's spent by transaction 1.
+            this.federationWalletManager.ProcessTransaction(transaction1);
+
+            // Confirm this can be done twice without ill effect.
+            // (We may have to re-reserve UTXO's associated with partial transactions after a node restart or re-org.)
+            this.federationWalletManager.ProcessTransaction(transaction1);
+
+            string opReturnData2 = blockHeight.ToString();
+
+            // Build the multisig transaction template.
+            var multiSigContext2 = new TransactionBuildContext(recipient2, opReturnData: Encoding.UTF8.GetBytes(opReturnData2))
+            {
+                TransactionFee = Money.Coins(0.01m),
+                MinConfirmations = 0,
+                Shuffle = false,
+                MultiSig = multiSigAddress,
+                IgnoreVerify = true,
+                Sign = false
+            };
+
+            // Build the transaction.
+            Transaction transaction2 = this.federationTransactionHandler.BuildTransaction(multiSigContext2);
+
+            // Transactions inputs.
+            Assert.Equal(2, transaction2.Inputs.Count);
+            Assert.Equal(transaction1.GetHash(), transaction2.Inputs[0].PrevOut.Hash);
+            Assert.Equal((uint)0, transaction2.Inputs[0].PrevOut.N);
+            Assert.Equal((uint256)2, transaction2.Inputs[1].PrevOut.Hash);
+            Assert.Equal((uint)0, transaction2.Inputs[1].PrevOut.N);
+
+            // Transaction outputs.
+            Assert.Equal(3, transaction2.Outputs.Count);
+
+            // Transaction output value - change.
+            Assert.Equal(new Money(9.98m, MoneyUnit.BTC), transaction2.Outputs[0].Value);
+            Assert.Equal(multiSigAddress.ScriptPubKey, transaction2.Outputs[0].ScriptPubKey);
+
+            // Transaction output value - recipient 2.
+            Assert.Equal(new Money(70m, MoneyUnit.BTC), transaction2.Outputs[1].Value);
+            Assert.Equal(recipient2[0].ScriptPubKey, transaction2.Outputs[1].ScriptPubKey);
+
+            // Transaction output value - op_return.
+            Assert.Equal(new Money(0m, MoneyUnit.BTC), transaction2.Outputs[2].Value);
+            Assert.Equal(opReturnData2, new OpReturnDataReader(this.loggerFactory, this.network).GetString(transaction2, out OpReturnDataType dummy2));
+        }
+
+        /// <summary>
+        /// Gets the path of the directory that <see cref="CreateTestDir(object, string)"/> or <see cref="CreateDataFolder(object, string)"/> would create.
+        /// </summary>
+        /// <remarks>The path of the directory is of the form TestCase/{testClass}/{testName}.</remarks>
+        /// <param name="caller">The calling object, from which we derive the namespace in which the test is contained.</param>
+        /// <param name="callingMethod">The name of the test being executed. A directory with the same name will be created.</param>
+        /// <returns>The path of the directory.</returns>
+        private static string GetTestDirectoryPath(object caller, [System.Runtime.CompilerServices.CallerMemberName] string callingMethod = "")
+        {
+            return Path.Combine(Path.GetTempPath() ?? ".", caller.GetType().Name, callingMethod);
         }
     }
 }
