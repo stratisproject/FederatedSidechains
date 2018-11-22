fdbe421d4457a3846e9f011471158523c78ab34a
Ferdeen Mughal <Ferdeen.Mughal@StratisPlatform.com>
2018-11-22 10:33:54 +0000

    Nodesettings changed to include FederatedPegNetwork.

--- src/Stratis.FederatedPeg.Tests/SignedMultisigTransactionBroadcasterTests.cs
+++ src/Stratis.FederatedPeg.Tests/SignedMultisigTransactionBroadcasterTests.cs
@@ -57,7 +57,7 @@ namespace Stratis.FederatedPeg.Tests
 
             // Setup MempoolManager.
             this.dateTimeProvider = Substitute.For<IDateTimeProvider>();
-            this.nodeSettings = NodeSettings.Default(ApexNetwork.Test);
+            this.nodeSettings = new NodeSettings(networksSelector: FederatedPegNetwork.NetworksSelector, protocolVersion: NBitcoin.Protocol.ProtocolVersion.ALT_PROTOCOL_VERSION);
 
             this.mempoolSettings = new MempoolSettings(this.nodeSettings)
             {
