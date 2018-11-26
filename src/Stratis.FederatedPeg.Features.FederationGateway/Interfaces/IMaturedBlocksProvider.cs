4dbb22100136446e1b7017856cbee5b51e5e0472
quantumagi <someguy.fromafrica@gmail.com>
2018-11-26 18:21:53 +1100

    Add MaturedBlocksProvider

---
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IMaturedBlocksProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
+{
+    public interface IMaturedBlocksProvider
+    {
+        Task<List<IMaturedBlockDeposits>> GetMaturedDepositsAsync(int blockHeight, int maxBlocks);
+    }
+}
