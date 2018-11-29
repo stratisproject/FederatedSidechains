f5ce377725e867b58adc0b59e7fd5614def25d10
quantumagi <someguy.fromafrica@gmail.com>
2018-11-29 19:40:21 +1100

    Move methods and add caching to IMaturedBlocksProvider

--- src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IMaturedBlocksProvider.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/IMaturedBlocksProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using NBitcoin;
 
 namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
 {
@@ -13,5 +14,20 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
         /// <returns>A list of mature block deposits.</returns>
         /// <exception cref="InvalidOperationException">Thrown if the blocks are not mature or not found.</exception>
         Task<List<IMaturedBlockDeposits>> GetMaturedDepositsAsync(int blockHeight, int maxBlocks);
+
+        /// <summary>
+        /// Gets all the available chained headers starting at the specified block height up to a maximum number of headers.
+        /// </summary>
+        /// <param name="blockHeight">The block height at which to start.</param>
+        /// <param name="maxHeaders">The maximum number of headers to get.</param>
+        /// <returns>All the available chained headers starting at the specified block height up to the maximum number of headers.</returns>
+        Task<List<ChainedHeader>> GetChainedHeadersAsync(int blockHeight, int maxHeaders);
+
+        /// <summary>
+        /// Gets deposits from the block that is expected to be mature given this chain header.
+        /// </summary>
+        /// <param name="chainedHeader">The last received chain header.</param>
+        /// <returns>The matured deposits.</returns>
+        IMaturedBlockDeposits ExtractMaturedBlockDeposits(ChainedHeader chainedHeader);
     }
 }
