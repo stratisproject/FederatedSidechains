731841ef79638b60aaf7620d798c64e1034e940d
quantumagi <someguy.fromafrica@gmail.com>
2018-11-29 20:20:37 +1100

    Reduce nesting

--- src/Stratis.FederatedPeg.Features.FederationGateway/TargetChain/EventsPersister.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/TargetChain/EventsPersister.cs
@@ -34,21 +34,21 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.TargetChain
         {
             this.logger.LogDebug("New {0} received.", nameof(IMaturedBlockDeposits));
 
-            if (maturedBlockDeposits?.Length > 0)
-            {
-                if (maturedBlockDeposits.Last().Block.BlockHeight >= this.store.NextMatureDepositHeight)
-                {
-                    for (int i = 0; i < maturedBlockDeposits.Length; i++)
-                    {
-                        if (maturedBlockDeposits[i].Block.BlockHeight == this.store.NextMatureDepositHeight)
-                        {
-                            await this.store.RecordLatestMatureDepositsAsync(maturedBlockDeposits[i].Deposits.ToArray()).ConfigureAwait(false);
-                        }
-                    }
+            if (maturedBlockDeposits?.Length <= 0)
+                return;
 
-                    await this.maturedBlocksRequester.GetMoreBlocksAsync().ConfigureAwait(false);
+            if (maturedBlockDeposits.Last().Block.BlockHeight < this.store.NextMatureDepositHeight)
+                return;
+
+            for (int i = 0; i < maturedBlockDeposits.Length; i++)
+            {
+                if (maturedBlockDeposits[i].Block.BlockHeight == this.store.NextMatureDepositHeight)
+                {
+                    await this.store.RecordLatestMatureDepositsAsync(maturedBlockDeposits[i].Deposits.ToArray()).ConfigureAwait(false);
                 }
             }
+
+            await this.maturedBlocksRequester.GetMoreBlocksAsync().ConfigureAwait(false);
         }
 
         /// <inheritdoc />
