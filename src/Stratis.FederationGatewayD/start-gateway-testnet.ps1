226d3620c8cada28364ec6002134b6f78c8a6dd4
Jeremy Bokobza <bokobza@gmail.com>
2018-12-09 21:32:47 +0000

    Added some sanity checks in the script Emptied the values that need to be added by the user so that they have to do it themselves

--- src/Stratis.FederationGatewayD/start-gateway-testnet.ps1
+++ src/Stratis.FederationGatewayD/start-gateway-testnet.ps1
@@ -1,30 +1,73 @@
 ﻿#############################
 #    UPDATE THESE VALUES    #
 #############################
-$git_repos_path = "C:\Users\matthieu\source\repos"
-$root_datadir = "C:\Users\matthieu\AppData\Roaming\StratisNode"
-$path_to_federationgatewayd = "$git_repos_path\FederatedSidechains\src\Stratis.FederationGatewayD"
+$root_datadir = ""     # Ex: C:\Users\matthieu\AppData\Roaming\StratisNode
+$path_to_federationgatewayd = "" # Ex: C:\Users\matthieu\source\repos\FederatedSidechains\src\Stratis.FederationGatewayD
 
-# please add the path to the federationKey.dat file generated when using federation setup tool.
-$path_to_mining_key_dat_file = "path to the federationKey.dat file"
+# Please add the path to the federationKey.dat file generated when using federation setup tool.
+$path_to_mining_key_dat_file = "" # Ex: C:\Users\matthieu\Documents\My federation\federationKey.dat
 
-$multisig_public_key = "03b824a9500f17c9fe7a4e3bb660e38b97b66ed3c78749146f2f31c06569cf905c"
-$mining_public_key = "0248de019680c6f18e434547c8c9d48965b656b8e5e70c5a5564cfb1270db79a11"
+$multisig_public_key = "" # Ex: 03b824a9500f17c9fe7a4e3bb660e38b97b66ed3c78749146f2f31c06569cf905c
+$mining_public_key = ""   # Ex: 0248de019680c6f18e434547c8c9d48965b656b8e5e70c5a5564cfb1270db79a11
 
-#keep this short
-$nickname = "matt"
+# Keep this short.
+$nickname = "" # Ex: matt
 
-$mainchain_federationips = "127.0.0.1:26178,127.0.0.2:26178,127.0.0.3:26178,127.0.0.4:26178,127.0.0.5:26178"
-$sidechain_federationips = "127.0.0.1:26179,127.0.0.2:26179,127.0.0.3:26179,127.0.0.4:26179,127.0.0.5:26179"
+# The list of ips of the other federated nodes. Please note the ports used to connect with other nodes.
+$mainchain_federationips = "" # Ex: 127.0.0.1:26178,127.0.0.2:26178,127.0.0.3:26178,127.0.0.4:26178,127.0.0.5:26178
+$sidechain_federationips = "" # Ex: 127.0.0.1:26179,127.0.0.2:26179,127.0.0.3:26179,127.0.0.4:26179,127.0.0.5:26179
 
 ######################################
 #    UPDATE THIS BUT DO NOT SHARE    #
 ######################################
-$multisig_mnemonic = "please change that to the keywords generated when using federation setup tool"
-# enter a password - used to protect your multisig wallet
-$multisig_password = "enter a password"
-# enter a different password - used to protect the wallet where poa rewards are sent
-$mining_wallet_password = "enter a password"
+
+# Please add the mnemonic generated when using federation setup tool.
+$multisig_mnemonic = "" #Ex: panther rich spend divert army squeeze surround thank spell entire dream split 
+
+# Enter a password - used to protect your multisig wallet.
+$multisig_password = "" # Ex: myP@55w0rd!
+
+# Enter a different password - used to protect the wallet where mining rewards are sent.
+$mining_wallet_password = "" # Ex: myP@55w0rd!
+
+######################################
+#         CHECK USER'S SETUP         #
+######################################
+if (!$root_datadir `
+	-or !$path_to_federationgatewayd `
+	-or !$path_to_mining_key_dat_file `
+	-or !$multisig_public_key `
+	-or !$mining_public_key `
+	-or !$nickname `
+	-or !$mainchain_federationips `
+	-or !$sidechain_federationips `
+    -or !$multisig_mnemonic `
+    -or !$multisig_password `
+    -or !$mining_wallet_password) 
+{ 
+    Write-Host "Error: Please make sure all the fields have been set." -foregroundcolor "Red"
+    Break
+}
+
+if( -not (Test-Path -Path $root_datadir))
+{
+	Write-Host "Error: The path to the StratisNode folder doesn't exist. Path entered: $root_datadir." -foregroundcolor "Red"
+	Break
+}
+
+if( -not (Test-Path -Path $path_to_federationgatewayd))
+{
+	Write-Host "Error: The path to the FederationGatewayD project doesn't exist. Path entered: $path_to_federationgatewayd." -foregroundcolor "Red"
+	Break
+}
+
+if(![System.IO.File]::Exists($path_to_mining_key_dat_file))
+{
+	Write-Host "Error: A file named FederationKey.dat couldn't be found at path '$path_to_mining_key_dat_file'." -foregroundcolor "Red"
+	Break
+}
+
+# TODO: add more checks
 
 
 #######################################
@@ -67,7 +110,7 @@ $agent_prefix = $nickname + "-" + $mining_public_key.Substring(0,5)
 cd $path_to_federationgatewayd
 # Federation member main and side
 Write-Host "Starting mainchain gateway node"
-start-process cmd -ArgumentList "/k color 09 && dotnet run -mainchain -testnet -agentprefix=$agent_prefix -datadir=$root_datadir\gateway -apiport=38221 -counterchainapiport=38222 -federationips=$mainchain_federationips -redeemscript=""$redeemscript"" -publickey=$multisig_public_key -mincoinmaturity=1 -mindepositconfirmations=1 -txindex=1"
+start-process cmd -ArgumentList "/k color 09 && dotnet run -mainchain -testnet -agentprefix=$agent_prefix -datadir=$root_datadir\gateway -apiport=38221 -counterchainapiport=38222 -federationips=$mainchain_federationips -redeemscript=""$redeemscript"" -publickey=$multisig_public_key -mincoinmaturity=1 -mindepositconfirmations=1"
 timeout $long_interval_time
 Write-Host "Starting sidechain gateway node"
 start-process cmd -ArgumentList "/k color 0D && dotnet run -sidechain -testnet -agentprefix=$agent_prefix -datadir=$root_datadir\gateway -apiport=38222 -counterchainapiport=38221 -federationips=$sidechain_federationips -redeemscript=""$redeemscript"" -publickey=$multisig_public_key -mincoinmaturity=1 -mindepositconfirmations=1 -txindex=1"
