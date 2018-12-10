c62e92196be5647da2769d2bb4e6889bd75c8073
monsieurleberre <monsieurleberre@users.noreply.github.com>
2018-12-10 09:56:01 +0000

    typo

--- src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/ICrossChainTransferStore.cs
+++ src/Stratis.FederatedPeg.Features.FederationGateway/Interfaces/ICrossChainTransferStore.cs
@@ -45,7 +45,7 @@ namespace Stratis.FederatedPeg.Features.FederationGateway.Interfaces
         /// Returns transactions by status. Orders the results by UTXO selection order.
         /// </summary>
         /// <param name="status">The status to get the transactions for.</param>
-        /// <param name="sort">Set to <c>true</c> to sort the transafers by their earliest inputs.</param>
+        /// <param name="sort">Set to <c>true</c> to sort the transfers by their earliest inputs.</param>
         /// <returns>An array of transactions.</returns>
         Task<Dictionary<uint256, Transaction>> GetTransactionsByStatusAsync(CrossChainTransferStatus status, bool sort = false);
 
