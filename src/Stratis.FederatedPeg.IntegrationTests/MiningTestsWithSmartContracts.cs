3276b67d948680a53707c80516d2619e633dd145
Jordan Andrews <jordandrews@hotmail.com>
2018-12-11 22:02:06 +1100

    2 Tests

--- src/Stratis.FederatedPeg.IntegrationTests/MiningTestsWithSmartContracts.cs
+++ src/Stratis.FederatedPeg.IntegrationTests/MiningTestsWithSmartContracts.cs
@@ -19,14 +19,30 @@ namespace Stratis.FederatedPeg.IntegrationTests
 {
     public class MiningTestsWithSmartContracts : TestBase
     {
-        //[Fact]
-        //public void Premine_Received_To_Federation()
-        //{
-        //    using (SidechainNodeBuilder builder = SidechainNodeBuilder.CreateSidechainNodeBuilder(this))
-        //    {
+        [Fact]
+        public void Premine_Received_To_Federation()
+        {
+            using (SidechainNodeBuilder builder = SidechainNodeBuilder.CreateSidechainNodeBuilder(this))
+            {
+                // Set up builder and node
+                builder.ConfigParameters.Add("sidechain", "true");
+                builder.ConfigParameters.Add("redeemscript", this.scriptAndAddresses.payToMultiSig.ToString());
+                builder.ConfigParameters.Add("publickey", this.pubKeysByMnemonic[this.mnemonics[0]].ToString());
+                CoreNode node = builder.CreateSidechainNodeWithSmartContracts(this.sidechainNetwork, this.sidechainNetwork.FederationKeys[0]);
 
-        //    }
-        //}
+                node.Start();
+
+                // Wait for node to reach premine height 
+                TestHelper.WaitLoop(() => node.FullNode.Chain.Height == node.FullNode.Network.Consensus.PremineHeight);
+
+                // Ensure that coinbase contains premine reward and it goes to the fed.
+                Block block = node.FullNode.Chain.Tip.Block;
+                Transaction coinbase = block.Transactions[0];
+                Assert.Single(coinbase.Outputs);
+                Assert.Equal(node.FullNode.Network.Consensus.PremineReward, coinbase.Outputs[0].Value);
+                Assert.Equal(this.scriptAndAddresses.payToMultiSig, coinbase.Outputs[0].ScriptPubKey);
+            }
+        }
 
 
         [Fact]
