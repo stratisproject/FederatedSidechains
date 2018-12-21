8db43be75e1c6c468e802010daf2c33c2984b446
noescape0 <NoEscape0@ya.ru>
2018-12-21 15:11:49 +0300

    Added 3 tests

--- src/Stratis.FederatedPeg.Tests/ControllersTests/FederationWalletControllerTests.cs
+++ src/Stratis.FederatedPeg.Tests/ControllersTests/FederationWalletControllerTests.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
 using NSubstitute;
 using Stratis.Bitcoin.Connection;
+using Stratis.Bitcoin.Features.Wallet;
+using Stratis.Bitcoin.Features.Wallet.Models;
 using Stratis.Bitcoin.Networks;
+using Stratis.Bitcoin.P2P.Peer;
+using Stratis.Bitcoin.Tests.Common;
 using Stratis.Bitcoin.Utilities;
 using Stratis.FederatedPeg.Features.FederationGateway.Controllers;
 using Stratis.FederatedPeg.Features.FederationGateway.Interfaces;
+using Stratis.FederatedPeg.Features.FederationGateway.Models;
+using Stratis.FederatedPeg.Features.FederationGateway.Wallet;
 using Xunit;
 
 namespace Stratis.FederatedPeg.Tests.ControllersTests
@@ -25,6 +32,7 @@ namespace Stratis.FederatedPeg.Tests.ControllersTests
         private readonly IWithdrawalHistoryProvider withdrawalHistoryProvider;
 
         private readonly FederationWalletController controller;
+        private readonly FederationWallet fedWallet;
 
         public FederationWalletControllerTests()
         {
@@ -33,18 +41,88 @@ namespace Stratis.FederatedPeg.Tests.ControllersTests
             this.walletSyncManager = Substitute.For<IFederationWalletSyncManager>();
             this.connectionManager = Substitute.For<IConnectionManager>();
             this.network = new StratisTest();
-            this.chain = Substitute.For<ConcurrentChain>();
+
+            this.chain = new ConcurrentChain(this.network);
+
+            ChainedHeader tip = ChainedHeadersHelper.CreateConsecutiveHeaders(100, ChainedHeadersHelper.CreateGenesisChainedHeader(this.network), true, null, this.network).Last();
+            this.chain.SetTip(tip);
+
+
             this.dateTimeProvider = Substitute.For<IDateTimeProvider>();
             this.withdrawalHistoryProvider = Substitute.For<IWithdrawalHistoryProvider>();
 
             this.controller = new FederationWalletController(this.loggerFactory, this.walletManager, this.walletSyncManager,
                 this.connectionManager, this.network, this.chain, this.dateTimeProvider, this.withdrawalHistoryProvider);
+
+            this.fedWallet = new FederationWallet();
+            this.fedWallet.Network = this.network;
+            this.fedWallet.LastBlockSyncedHeight = 999;
+            this.fedWallet.CreationTime = DateTimeOffset.Now;
+
+            this.walletManager.GetWallet().Returns(this.fedWallet);
         }
 
         [Fact]
-        public void QWE()
+        public void GetGeneralInfo()
+        {
+            this.connectionManager.ConnectedPeers.Returns(info => new NetworkPeerCollection());
+
+            IActionResult result = this.controller.GetGeneralInfo();
+            WalletGeneralInfoModel model = ActionResultToModel<WalletGeneralInfoModel>(result);
+
+            Assert.Equal(this.fedWallet.CreationTime, model.CreationTime);
+            Assert.Equal(this.fedWallet.LastBlockSyncedHeight, model.LastBlockSyncedHeight);
+            Assert.Equal(this.fedWallet.Network, model.Network);
+        }
+
+        [Fact]
+        public void GetBalance()
+        {
+            this.fedWallet.MultiSigAddress = new MultiSigAddress();
+
+            IActionResult result = this.controller.GetBalance();
+            WalletBalanceModel model = ActionResultToModel<WalletBalanceModel>(result);
+
+            Assert.Single(model.AccountsBalances);
+            Assert.Equal(CoinType.Stratis, model.AccountsBalances.First().CoinType);
+            Assert.Equal(0, model.AccountsBalances.First().AmountConfirmed.Satoshi);
+        }
+
+        [Fact]
+        public void GetHistory()
+        {
+            var withdrawals = new List<WithdrawalModel>() {new WithdrawalModel(), new WithdrawalModel()};
+
+            this.withdrawalHistoryProvider.GetHistory(0).ReturnsForAnyArgs(withdrawals);
+
+            IActionResult result = this.controller.GetHistory(5);
+            List<WithdrawalModel> model = ActionResultToModel<List<WithdrawalModel>>(result);
+
+            Assert.Equal(withdrawals.Count, model.Count);
+        }
+
+        [Fact]
+        public void Sync()
         {
 
+        }
+
+        [Fact]
+        public void EnableFederation()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveTransactions()
+        {
+
+        }
+
+        private T ActionResultToModel<T>(IActionResult result) where T : class
+        {
+            T model = (result as JsonResult).Value as T;
+            return model;
         }
     }
 }
